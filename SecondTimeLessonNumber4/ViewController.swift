8c231924e5e45480d37e691fc075ced1e3567c78
Vladimir <kynicin87@mail.ru>
2020-08-19 23:12:47 +0300

    Initial Commit

---
+++ SecondTimeLessonNumber4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondTimeLessonNumber4
+//
+//  Created by vladimir gennadievich on 19.08.2020.
+//  Copyright © 2020 Vladimir Gennadievich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
